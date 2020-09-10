d82db1a0da5b3ce0fd6bd718af3f042af6d2f241
koriel50000 <JZW00040@gmail.com>
2020-09-11 07:22:07 +0900

    一旦現状をコミット

--- src/main/python/reversi_inference.py
+++ src/main/python/reversi_inference.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import glob
 import os.path
+import zipfile
 
 import reversi
 import converter
@@ -11,15 +13,24 @@ from tensorflow import keras
 model = keras.models.load_model("../resources/model/")
 
 # global変数宣言
-num_total = 0
-num_correct = 0
+total_accuracy = 0.0
+move_accuracies = [0.0] * 60
+
+
+#
+# 予測値を計算する
+#
+def calculate_predicted_value(state):
+    state_ = state.reshape(1, -1)
+    predictions_ = model.predict(state_)
+    return predictions_[0][0]
 
 
 #
 # 棋譜を評価する
 #
-def evaluate_records(sess, move_record, eval_record):
-    global num_total, num_correct
+def evaluating_records(move_record, eval_record):
+    global total_accuracy, move_accuracies
 
     reversi.initialize()
 
@@ -32,11 +43,14 @@ def evaluate_records(sess, move_record, eval_record):
             coord = entry['coord']
             value = entry['value']
             state = converter.convert_state(reversi, coord)
-            predicted_value = cnn_model.calculate_predicted_value(sess, state)
+            predicted_value = calculate_predicted_value(state)(state)
             values.append(value)
             predicted_values.append(predicted_value)
             # probs = np.r_[pprobs, nprobs]
             # view_probs = ["{0:.2f}".format(x) for x in probs]
+
+        epsilon = 0.001
+
         for value in values:
             print(' {0:.2f}'.format(value), end='')
         print()
@@ -44,10 +58,7 @@ def evaluate_records(sess, move_record, eval_record):
             print(' {0:.2f}'.format(value), end='')
         print()
         print()
-        num_total += 1
-        if predicted_value == value:
-            num_correct += 1
-        # 評価値がdelta範囲内で降順、同値は着手のindexで昇順に並び替え
+        # 評価値がepsilon範囲内で降順、同値は棋譜の着手順
         # 正答率は評価値の上位3つで計算する
         # 1 2 3 : 1.0
         # 1 3 2 : 0.9
@@ -60,6 +71,29 @@ def evaluate_records(sess, move_record, eval_record):
         # 3 x x : 0.1
         # x x x : 0.0 上記以外は0.0
         # まずはこれでやってみる
+        if predicted_values[0] == values[0]:
+            if predicted_values[1] == values[2]:
+                accuracy = 1.0
+            elif predicted_values[2] == values[1]:
+                accuracy = 0.9
+            else:
+                accuracy = 0.8
+        elif predicted_values[1] == values[1]:
+            if predicted_values[1] == values[2]:
+                accuracy = 0.7
+            elif predicted_values[2] == values[1]:
+                accuracy = 0.6
+            else:
+                accuracy = 0.4
+        elif predicted_values[2] == values[2]:
+            if predicted_values[1] == values[2]:
+                accuracy = 0.3
+            elif predicted_values[2] == values[1]:
+                accuracy = 0.2
+            else:
+                accuracy = 0.1
+        else:
+            accuracy = 0.0
 
         coord = converter.move_to_coord(actual_move)
         reversi.make_move(coord)
@@ -69,25 +103,44 @@ def evaluate_records(sess, move_record, eval_record):
 #
 # 学習モデルを評価する
 #
-def evaluating_model(sess, path, filenames, begin, end):
-    for i in range(begin, end):
-        file = os.path.join(path, filenames[i])
+def evaluating_model(path, filenames):
+    for i, filename in enumerate(filenames):
+        file = os.path.join(path, filename)
         with open(file, "r") as fin:
             move_record = fin.readline().strip()
             eval_record = [x.strip() for x in fin.readlines()]
 
-        evaluate_records(sess, move_record, eval_record)
+        evaluating_records(move_record, eval_record)
+
         # 全体の正答率
-        # depth毎の正答率
         if i % 10 == 0:
             print("{0}: {1}".format(i, file))
-            print(float(num_correct) / num_total)
+            print("total_accuracy: {0}".format(total_accuracy))
+
+    # 全体の正答率
+    print("total_accuracy: {0}".format(total_accuracy))
+    # depth毎の正答率
+    for i in range(len(move_accuracies)):
+        print("move_accuracies[{0}]: {1}".format(i, move_accuracies[i]))
 
 
 #
 # メイン
 #
 def main(args):
+    path = "../resources/records/"
+    with zipfile.ZipFile(os.path.join(path, "kifu102245.zip")) as records_zip:
+
+        namelist = records_zip.namelist()
+        step = 1000
+        for i, pos in enumerate(range(100000, 102000, step)):
+            filenames = namelist[pos: pos + step]
+            records_zip.extractall(path, filenames)
+
+            evaluating_model(path, filenames)
+
+            for file in glob.glob(os.path.join(path, "kifu*.txt")):
+                os.remove(file)
 
     return 0
 
